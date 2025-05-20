751e1c5413f57edac8a9e9fcb436c202920e49fd
Bibek Ghale Pariyar <bibek.gp@paymentology.com>
2025-05-20 20:50:23 +0545

    add shared-ui

---
+++ src/libs/shared-ui/dist/components/shared/Card.js
@@ -0,0 +1,80 @@
+import { jsx } from "react/jsx-runtime";
+import React from "react";
+import { cn } from "../../lib/utils.js";
+const Card = React.forwardRef(
+  ({ className, children, variant = "default", padding = "md", ...props }, ref) => {
+    const variantStyles = {
+      default: "bg-white dark:bg-gray-800",
+      bordered: "bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700",
+      elevated: "bg-white dark:bg-gray-800 shadow-md"
+    };
+    const paddingStyles = {
+      none: "",
+      sm: "p-3",
+      md: "p-5",
+      lg: "p-7"
+    };
+    return /* @__PURE__ */ jsx(
+      "div",
+      {
+        className: cn(
+          "rounded-lg",
+          variantStyles[variant],
+          paddingStyles[padding],
+          className
+        ),
+        ref,
+        ...props,
+        children
+      }
+    );
+  }
+);
+Card.displayName = "Card";
+const CardHeader = React.forwardRef(({ className, ...props }, ref) => /* @__PURE__ */ jsx(
+  "div",
+  {
+    ref,
+    className: cn("flex flex-col space-y-1.5 mb-4", className),
+    ...props
+  }
+));
+CardHeader.displayName = "CardHeader";
+const CardTitle = React.forwardRef(({ className, ...props }, ref) => /* @__PURE__ */ jsx(
+  "h3",
+  {
+    ref,
+    className: cn("text-lg font-semibold", className),
+    ...props
+  }
+));
+CardTitle.displayName = "CardTitle";
+const CardDescription = React.forwardRef(({ className, ...props }, ref) => /* @__PURE__ */ jsx(
+  "p",
+  {
+    ref,
+    className: cn("text-sm text-gray-500 dark:text-gray-400", className),
+    ...props
+  }
+));
+CardDescription.displayName = "CardDescription";
+const CardContent = React.forwardRef(({ className, ...props }, ref) => /* @__PURE__ */ jsx("div", { ref, className: cn("", className), ...props }));
+CardContent.displayName = "CardContent";
+const CardFooter = React.forwardRef(({ className, ...props }, ref) => /* @__PURE__ */ jsx(
+  "div",
+  {
+    ref,
+    className: cn("flex items-center mt-4 pt-4 border-t", className),
+    ...props
+  }
+));
+CardFooter.displayName = "CardFooter";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+};
+//# sourceMappingURL=Card.js.map
---
+++ src/libs/shared-ui/dist/components/shared/index.js
@@ -0,0 +1,25 @@
+import { Button } from "./Button.js";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./Card.js";
+import { Input } from "./Input.js";
+import { Badge } from "./Badge.js";
+import { Alert } from "./Alert.js";
+import { Heading, Text } from "./Typography.js";
+import { ThemeToggle } from "./ThemeToggle.js";
+import { cn } from "../../lib/utils.js";
+export {
+  Alert,
+  Badge,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Heading,
+  Input,
+  Text,
+  ThemeToggle,
+  cn
+};
+//# sourceMappingURL=index.js.map
